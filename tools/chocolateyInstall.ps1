284990524a1a987c77da947de9f732769045c4fb
Marc Sutter <marc.sutter@swisscom.com>
2015-04-18 15:19:33 +0200

    use standart install script

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,37 +1,18 @@
-#------- INSTALLATION -------#
+# this file was automatically generated with the Powershell scPackageTools module
+
+$packageName = "chocolateypkghelpers"
+$moduleName = "chocolateyPkgHelpers"
+
 try {
-  # Set the location of the package on disk
-  $PackagePath = Split-Path -Parent (Split-Path -Parent $MyInvocation.MyCommand.Path)
+  $installDir = Join-Path $PSHome "Modules"
+  $myDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+  $pkgpath = (get-item $myDir).parent.FullName
+  $psmodulepath = "$pkgpath/files"
+  Copy-Item $psmodulepath $installDir/$modulename -Recurse -Force
 
-  # Include chocolateyPkgHelpers Module
-  Import-Module -Name "${PackagePath}\tools\chocolateyPkgHelpers\chocolateyPkgHelpers.psm1" -Force
-
-  # Load Package Datas
-  $ChocoPkgData = Get-ChocoPkgData -PackagePath $PackagePath
-  Write-Host "ChocoPkgData: ${ChocoPkgData}"
-
-  #------- EXECUTE BEFORE-INSTALL SCRIPT WHEN PRESENT ---------#
-  $BeforeInstallPath = "$($ChocoPkgData.ToolsPath)\chocolateyBeforeInstall.ps1"
-  if (Test-Path -Path "${BeforeInstallPath}") {
-    Write-Verbose "Executing ${BeforeInstallPath}"
-    & "${BeforeInstallPath}"
-  }
-
-  #------- INSTALLATION SETUP (Only files) ---------#
-  Install-ChocoPkgFiles $ChocoPkgData
-  Install-ChocoPkgInstallers $ChocoPkgData
-
-
-  #------- EXECUTE AFTER-INSTALL SCRIPT WHEN PRESENT ---------#
-  $AfterInstallPath = "$($ChocoPkgData.ToolsPath)\chocolateyAfterInstall.ps1"
-  if (Test-Path -Path "${AfterInstallPath}") {
-    Write-Verbose "Executing ${AfterInstallPath}"
-    & "${AfterInstallPath}"
-  }
-
-  #------- DONE -----------------------#
-  Write-ChocolateySuccess $ChocoPkgData.PackageId
+  Write-ChocolateySuccess "$packageName"
 } catch {
-  Write-ChocolateyFailure $ChocoPkgData.PackageId $($_.Exception.Message)
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
   throw
 }
+
--- tools/chocolateyUninstall.ps1
+++ tools/chocolateyUninstall.ps1
@@ -1,36 +1,11 @@
-#------- UNINSTALLATION -------#
+$packageName = "chocolateypkghelpers"
+$moduleName = "chocolateyPkgHelpers"
+
 try {
-  # Set the location of the package on disk
-  $PackagePath = Split-Path -Parent (Split-Path -Parent $MyInvocation.MyCommand.Path)
-  Write-Host "PackagePath: ${PackagePath}"
-
-  # Include chocolateyPkgHelpers Module
-  Import-Module -Name "${PackagePath}\tools\chocolateyPkgHelpers\chocolateyPkgHelpers.psm1" -Force
-
-  # Load Package Datas
-  $ChocoPkgData = Get-ChocoPkgData -PackagePath $PackagePath
-
-  #------- BEFORE UNINSTALL SCRIPT ---------#
-  $BeforeUninstallPath = "$($ChocoPkgData.ToolsPath)\chocolateyBeforeUninstall.ps1"
-  if (Test-Path -Path "${BeforeUninstallPath}") {
-    Write-Verbose "Executing ${BeforeUninstallPath}"
-    & "${BeforeUninstallPath}"
-  }
-
-  #------- UNINSTALLATION SETUP ---------#
-  Uninstall-ChocoPkgUninstallers $ChocoPkgData
-  Uninstall-ChocoPkgFiles $ChocoPkgData
-
-  #------- AFTER UNINSTALL SCRIPT ---------#
-  $AfterUninstallPath = "$($ChocoPkgData.ToolsPath)\chocolateyAfterUninstall.ps1"
-  if (Test-Path -Path "${AfterUninstallPath}") {
-    Write-Verbose "Executing ${AfterUninstallPath}"
-    & "${AfterUninstallPath}"
-  }
-
-  #------- DONE -----------------------#
-  Write-ChocolateySuccess $ChocoPkgData.PackageId
+  $installPath = Join-Path $PSHome  "Modules\$modulename"
+  Remove-Item -Recurse -Force $installPath
+  Write-ChocolateySuccess "$packageName"
 } catch {
-  Write-ChocolateyFailure $ChocoPkgData.PackageId $($_.Exception.Message)
+  Write-ChocolateyFailure "$packageName" $($_.Exception.Message)
   throw
 }
