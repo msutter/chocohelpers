43423876270a974f30e388f6ead6d30764a6d25d
Marc Sutter <marc.sutter@swisscom.com>
2015-04-25 16:25:00 +0200

    add a get-puppetrole function

---
+++ functions/Get-PuppetRole.ps1
@@ -0,0 +1,21 @@
+Function Get-PuppeRole
+{
+    [CmdletBinding()]
+    Param
+    (
+        [ValidateScript( {Test-Path($_) } )]
+        [Parameter(Mandatory = $false)]
+        [string]$PuppetRolePath = 'C:\ProgramData\PuppetLabs\facter\facts.d\role.txt'
+    )
+
+    #set custom propertie 1 to puppet role else set it to '$false'
+    if(Test-Path $PuppetRolePath){
+        $puppetrole = Select-String -Path $PuppetRolePath -SimpleMatch "configured_role="
+        $puppetrole = $puppetrole.Line.substring(16)
+    } else {
+        $puppetrole = $false
+    }
+
+    $puppetrole
+
+}
